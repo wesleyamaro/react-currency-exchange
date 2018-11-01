fa5c23d093ea23fb8e18674e5ba44cf4f4b7990e
Wesley Amaro <contato@wesleyamaro.com>
2018-11-01 00:25:44 -0300

    feat: creating store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers/';
 import App from './containers/Home/';
 import registerServiceWorker from './registerServiceWorker';
+
 import './assets/styles/app.sass';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk, logger)));
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
+
 registerServiceWorker();
