1695c15d63b6f3bcf231c1b15b7d1a788c6ba102
Wesley Amaro <contato@wesleyamaro.com>
2018-11-01 00:26:08 -0300

    feat: creating currency exchange container

--- src/containers/CurrencyExchange/index.jsx
+++ src/containers/CurrencyExchange/index.jsx
@@ -1,19 +1,99 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { convertCurrency } from '../../actions/currencyExchange';
+import { currency} from '../../helpers/currency';
+import { connect } from 'react-redux';
 import './index.sass';
 
-const CurrencyExchange = () => {
-	return (
-		<section id="currency-exchange">
-			<form>
-				<input type="text" className="col-4 grid-margin" placeholder="USD" />
-				<input type="text" className="col-4 grid-margin" placeholder="EU" />
+class CurrencyExchange extends React.Component {
+	constructor(props) {
+		super(props);
 
-				<div id="actions">
-					<button>Convert</button>
-				</div>
-			</form>
-		</section>
-	);
+		this.state = {
+			txtEuro: '',
+			buttonDisabled: false
+		};
+
+		this.onHandleChange = this.onHandleChange.bind(this);
+		this.onSubmitConvertCurrency = this.onSubmitConvertCurrency.bind(this);
+	}
+
+	onHandleChange(e) {
+		const reg = /^[0-9]+$/g;
+
+		if(reg.test(e.target.value) || e.target.value === '') {
+			this.setState({
+				[e.target.name]: e.target.value
+			});
+		}
+	}
+
+	onSubmitConvertCurrency(e) {
+		e.preventDefault();
+
+		const { convertCurrency } = this.props;
+		const { txtEuro } = this.state;
+
+		this.setState({ buttonDisabled: true });
+
+		convertCurrency(txtEuro)
+			.then(() => {
+				this.setState({ buttonDisabled: false });
+			})
+			.catch(() => {
+				this.setState({ buttonDisabled: false });
+				alert('Something went wrong! Try again!');
+			});
+	}
+
+	render() {
+		const { txtDolar } = this.props;
+		const { txtEuro, buttonDisabled } = this.state;
+
+		return (
+			<section id="currency-exchange">
+				<form onSubmit={this.onSubmitConvertCurrency}>
+					<div className="row col-6 grid-margin">
+						<label>Euro (€):</label>
+						<input
+							type="text"
+							placeholder="EUR"
+							onChange={this.onHandleChange}
+							value={txtEuro !== '' ? txtEuro : ''}
+							name="txtEuro"
+							required
+						/>
+					</div>
+
+					<div className="row col-6 grid-margin">
+						<label>Dólar ($):</label>
+						<input
+							type="text"
+							placeholder="USD"
+							readOnly
+							value={txtDolar === 0 ? '' : currency(parseFloat(txtDolar))}
+							name="euText"
+						/>
+					</div>
+
+					<div id="actions">
+						<button disabled={buttonDisabled}>Convert</button>
+					</div>
+				</form>
+			</section>
+		);
+	}
 }
 
-export default CurrencyExchange;
+const mapStateToProps = (state) => {
+	return {
+		txtDolar: state.currencyReducer.dolar
+	};
+};
+
+CurrencyExchange.propTypes = {
+	txtDolar: PropTypes.number,
+	convertCurrency: PropTypes.func
+};
+
+export default connect(mapStateToProps, { convertCurrency })(CurrencyExchange);
