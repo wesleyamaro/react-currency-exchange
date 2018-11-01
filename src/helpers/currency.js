2ab2fa0629478bb3c5f3fda8734a7e6a95e1914c
Wesley Amaro <contato@wesleyamaro.com>
2018-11-01 09:43:20 -0300

    fix

--- src/helpers/currency.js
+++ src/helpers/currency.js
@@ -1,3 +1,9 @@
-export function currency(n) {
-	return n.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+export function currency(_number, _locale = 'en-US', _format = 'USD') {
+	const formatter = new Intl.NumberFormat(_locale, {
+		style: 'currency',
+		currency: _format,
+		minimumFractionDigits: 2
+	});
+
+	return formatter.format(_number);
 }
